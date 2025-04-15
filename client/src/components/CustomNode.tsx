2730e70f6985f22ee433fbdd5f44a3f831981b91
JerryHao <41164699+JerryHao2001@users.noreply.github.com>
2025-04-15 22:01:12 +0000

    Fix edge direction

--- client/src/components/CustomNode.tsx
+++ client/src/components/CustomNode.tsx
@@ -1,67 +1,67 @@
-import { memo } from 'react';
-import { Handle, Position, NodeProps } from 'reactflow';
-import { CustomNodeData } from '../types';
+import { memo } from "react";
+import { Handle, Position, NodeProps } from "reactflow";
+import { CustomNodeData } from "../types";
 
 const CustomNode = ({ data, selected, id }: NodeProps<CustomNodeData>) => {
   return (
     <div
       className={`px-3 py-2 rounded-lg bg-white border-2 ${
-        selected ? 'border-crimson shadow-md' : 'border-crimson/70'
+        selected ? "border-crimson shadow-md" : "border-crimson/70"
       } min-w-[150px] max-w-[200px] shadow hover:shadow-md transition-shadow duration-200 relative`}
     >
       {/* One handle on each side that works as both source and target */}
-      <Handle 
-        type="source" 
-        position={Position.Top} 
-        className="w-3 h-3 bg-crimson" 
+      <Handle
+        type="target"
+        position={Position.Top}
+        className="w-3 h-3 bg-crimson"
         id="top"
       />
-      <Handle 
-        type="target" 
-        position={Position.Top} 
-        className="w-3 h-3 bg-crimson opacity-0" 
+      <Handle
+        type="source"
+        position={Position.Top}
+        className="w-3 h-3 bg-crimson opacity-0"
         id="top"
       />
-      
-      <Handle 
-        type="source" 
-        position={Position.Right} 
-        className="w-3 h-3 bg-crimson" 
+
+      <Handle
+        type="target"
+        position={Position.Right}
+        className="w-3 h-3 bg-crimson"
         id="right"
       />
-      <Handle 
-        type="target" 
-        position={Position.Right} 
-        className="w-3 h-3 bg-crimson opacity-0" 
+      <Handle
+        type="source"
+        position={Position.Right}
+        className="w-3 h-3 bg-crimson opacity-0"
         id="right"
       />
-      
-      <Handle 
-        type="source" 
-        position={Position.Bottom} 
-        className="w-3 h-3 bg-crimson" 
+
+      <Handle
+        type="target"
+        position={Position.Bottom}
+        className="w-3 h-3 bg-crimson"
         id="bottom"
       />
-      <Handle 
-        type="target" 
-        position={Position.Bottom} 
-        className="w-3 h-3 bg-crimson opacity-0" 
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        className="w-3 h-3 bg-crimson opacity-0"
         id="bottom"
       />
-      
-      <Handle 
-        type="source" 
-        position={Position.Left} 
-        className="w-3 h-3 bg-crimson" 
+
+      <Handle
+        type="target"
+        position={Position.Left}
+        className="w-3 h-3 bg-crimson"
         id="left"
       />
-      <Handle 
-        type="target" 
-        position={Position.Left} 
-        className="w-3 h-3 bg-crimson opacity-0" 
+      <Handle
+        type="source"
+        position={Position.Left}
+        className="w-3 h-3 bg-crimson opacity-0"
         id="left"
       />
-      
+
       {/* Content */}
       <div className="font-semibold text-crimson">{data.label}</div>
       {data.description && (
